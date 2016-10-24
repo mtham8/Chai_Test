e80cfa13e80a5d17b7db6662ae29c4c7ac058fe2
Michelle Tham <mtham101@gmail.com>
2016-10-24 00:37:01 -0700

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(express.static('../client'));
 app.use(bodyParser());
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
 const port = process.env.PORT || 8080;
